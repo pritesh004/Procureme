cc567264110bf873de6eebfdcf96738c7e66ca37
HP <jawalkaranu24@gmail.com>
2020-05-26 15:13:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/procurement/ui/main/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.procurement.ui.main;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.widget.Button;
+
+import com.example.procurement.R;
+
+public class MainActivity extends
+    Button btnActivity1;
+    Button btnActivity2;
+    Button btnActivity3;
+    Button btnActivity4;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_Activity );
+    }
+
+
+
+}
